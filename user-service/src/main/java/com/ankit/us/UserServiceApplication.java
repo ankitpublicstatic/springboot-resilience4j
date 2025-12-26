87de8ab457dfd3f612590f1579dd75c673c1d9bf
ankitpublicstatic <ankitpublicstatic@gmail.com>
2025-12-26 14:58:34 +0530

    Updated

--- user-service/src/main/java/com/ankit/us/UserServiceApplication.java
+++ user-service/src/main/java/com/ankit/us/UserServiceApplication.java
@@ -1,8 +1,10 @@
 package com.ankit.us;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
-import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
-import io.github.resilience4j.retry.annotation.Retry;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,56 +16,67 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import com.ankit.us.dto.OrderDTO;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 
 @SpringBootApplication
 @RestController
 @RequestMapping("/user-service")
 public class UserServiceApplication {
 
-    @Autowired
-    @Lazy
-    private RestTemplate restTemplate;
+  @Autowired
+  @Lazy
+  private RestTemplate restTemplate;
 
-    public static final String USER_SERVICE="userService";
+  public static final String USER_SERVICE = "userService";
 
-    private static final String BASEURL = "http://localhost:9191/orders";
+  private static final String BASEURL = "http://localhost:9191/orders";
 
-    private int attempt=1;
+  private int attempt = 0;
 
 
-    @GetMapping("/displayOrders")
-  // @CircuitBreaker(name =USER_SERVICE,fallbackMethod = "getAllAvailableProducts")
-    @Retry(name = USER_SERVICE,fallbackMethod = "getAllAvailableProducts")
-    public List<OrderDTO> displayOrders(@RequestParam("category") String category) {
-        String url = category == null ? BASEURL : BASEURL + "/" + category;
-        System.out.println("retry method called "+attempt++ +" times "+" at "+new Date());
-        return restTemplate.getForObject(url, ArrayList.class);
+  @GetMapping("/displayOrders")
+  @CircuitBreaker(name = USER_SERVICE, fallbackMethod = "getAllAvailableProducts")
+  @Retry(name = USER_SERVICE, fallbackMethod = "getAllAvailableProductsRetry")
+  public List<OrderDTO> displayOrders(@RequestParam("category") String category) {
+    String url = category == null ? BASEURL : BASEURL + "/" + category;
+    System.out.println("retry method called " + ++attempt + " times " + " at " + new Date());
+    ArrayList response = restTemplate.getForObject(url, ArrayList.class);
+    if (response != null && response.size() > 0) {
+      attempt = 0;
+      return response;
     }
+    return null;
+  }
 
 
-    public List<OrderDTO> getAllAvailableProducts(Exception e){
-        return Stream.of(
-                new OrderDTO(119, "LED TV", "electronics", "white", 45000),
-                new OrderDTO(345, "Headset", "electronics", "black", 7000),
-                new OrderDTO(475, "Sound bar", "electronics", "black", 13000),
-                new OrderDTO(574, "Puma Shoes", "foot wear", "black & white", 4600),
-                new OrderDTO(678, "Vegetable chopper", "kitchen", "blue", 999),
-                new OrderDTO(532, "Oven Gloves", "kitchen", "gray", 745)
-        ).collect(Collectors.toList());
-    }
+  public List<OrderDTO> getAllAvailableProducts(Exception e) {
+    System.out.println("fallback executed circuit breaker");
+    return Stream.of(new OrderDTO(119, "LED TV", "electronics", "white", 45000),
+        new OrderDTO(345, "Headset", "electronics", "black", 7000),
+        new OrderDTO(475, "Sound bar", "electronics", "black", 13000),
+        new OrderDTO(574, "Puma Shoes", "foot wear", "black & white", 4600),
+        new OrderDTO(678, "Vegetable chopper", "kitchen", "blue", 999),
+        new OrderDTO(532, "Oven Gloves", "kitchen", "gray", 745)).collect(Collectors.toList());
+  }
+
+  public List<OrderDTO> getAllAvailableProductsRetry(Exception e) {
+    System.out.println("fallback executed retry");
+    return Stream.of(new OrderDTO(119, "LED TV", "electronics", "white", 45000),
+        new OrderDTO(345, "Headset", "electronics", "black", 7000),
+        new OrderDTO(475, "Sound bar", "electronics", "black", 13000),
+        new OrderDTO(574, "Puma Shoes", "foot wear", "black & white", 4600),
+        new OrderDTO(678, "Vegetable chopper", "kitchen", "blue", 999),
+        new OrderDTO(532, "Oven Gloves", "kitchen", "gray", 745)).collect(Collectors.toList());
+  }
 
 
-    public static void main(String[] args) {
-        SpringApplication.run(UserServiceApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(UserServiceApplication.class, args);
+  }
 
-    @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
+  @Bean
+  public RestTemplate restTemplate() {
+    return new RestTemplate();
+  }
 }
